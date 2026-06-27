c504b5ff8ace6cfa88fdf5dc2792e6a3cc46e578
aaziy <121827899+aaziy@users.noreply.github.com>
2026-06-28 01:26:08 +0500

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -11,6 +11,8 @@ class DocumentSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "title",
+            "author",
+            "doc_date",
             "source_filename",
             "mime_type",
             "status",
@@ -28,6 +30,21 @@ class DocumentSerializer(serializers.ModelSerializer):
 class QuerySerializer(serializers.Serializer):
     question = serializers.CharField(min_length=1, trim_whitespace=True)
     k = serializers.IntegerField(required=False, min_value=1, max_value=20)
+    document_ids = serializers.ListField(
+        child=serializers.IntegerField(), required=False
+    )
+    author = serializers.CharField(required=False)
+    date_from = serializers.DateField(required=False)
+    date_to = serializers.DateField(required=False)
+
+    def to_filters(self) -> dict:
+        data = self.validated_data
+        return {
+            "document_ids": data.get("document_ids"),
+            "author": data.get("author"),
+            "date_from": data.get("date_from"),
+            "date_to": data.get("date_to"),
+        }
 
 
 class RegisterSerializer(serializers.Serializer):
